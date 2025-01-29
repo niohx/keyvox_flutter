c19403cea7041c8450ad57b3b1f0c6b4518c9348
niohx <nioh@live.jp>
2025-01-30 00:22:32 +0900

    Update test suite and add Mockito for HTTP client mocking
    - Refactor test to use Mockito for HTTP client mocking
    - Add @GenerateMocks annotation for http.Client
    - Update test case with more comprehensive mock response
    - Remove MockClient and use Mockito's generated mock
    - Add pubspec.yaml configuration for Material Design

--- test/keyvox_flutter_test.dart
+++ test/keyvox_flutter_test.dart
@@ -1,58 +1,70 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
-import 'package:http/testing.dart';
 import 'package:keyvox_flutter/keyvox_flutter.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'keyvox_flutter_test.mocks.dart';
 
+@GenerateMocks([http.Client])
 void main() {
   group('KeyvoxClient', () {
-    late KeyvoxClient client;
-    late MockClient mockClient;
-
-    setUp(() {
-      mockClient = MockClient((request) async {
-        if (request.url.path == '/v1/getUnits') {
-          return http.Response(
-            '''
-            {
-              "code": "0",
-              "msg": "success",
-              "data": [
-                {
-                  "unitId": "5de092a8ef16f9512c1f1fff",
-                  "unitName": "1101室",
-                  "lockIds": "5bdfdd421bca968a774ec18a,5bd80fcd31bca24fc30e1eab",
-                  "placeName": "内幸町ホテル",
-                  "unitState": "I",
-                  "unitType": "シングル",
-                  "placeType": "facility"
-                }
-              ]
-            }
-            ''',
-            200,
-          );
-        }
-        return http.Response('Not found', 404);
-      });
-
-      client = KeyvoxClient(
+    test('getUnits returns list of units', () async {
+      final client = MockClient();
+      final keyvoxClient = KeyvoxClient(
         apiKey: 'test_api_key',
         secretKey: 'test_secret_key',
-        httpClient: mockClient,
+        httpClient: client,
       );
-    });
 
-    test('getUnits returns list of units', () async {
-      final response = await client.getUnits();
+      final mockResponse = '''
+      {
+        "code": "0",
+        "msg": "success",
+        "data": [
+          {
+            "unitId": "unit001",
+            "placeName": "テストホテル",
+            "unitName": "101",
+            "unitType": "シングル",
+            "unitState": "D",
+            "lockIds": "LOCK001,LOCK002",
+            "placeType": "facility"
+          },
+          {
+            "unitId": "unit002",
+            "placeName": "テストホテル",
+            "unitName": "102",
+            "unitType": "ダブル",
+            "unitState": "I",
+            "lockIds": "LOCK003,LOCK004",
+            "placeType": "facility"
+          },
+          {
+            "unitId": "unit003",
+            "placeName": "テストホテル",
+            "unitName": "エントランス",
+            "unitState": "I",
+            "placeType": "facility"
+          }
+        ]
+      }
+      ''';
 
-      expect(response.code, equals('0'));
-      expect(response.msg, equals('success'));
-      expect(response.data, isNotNull);
-      expect(response.data!.length, equals(1));
+      when(client.get(
+        Uri.parse('https://api.keyvox.cn/v1/getUnits'),
+        headers: anyNamed('headers'),
+      )).thenAnswer((_) async => http.Response(mockResponse, 200));
 
-      final unit = response.data!.first;
-      expect(unit.unitId, equals('5de092a8ef16f9512c1f1fff'));
-      expect(unit.unitName, equals('1101室'));
+      final units = await keyvoxClient.getUnits();
+
+      expect(units.code, '0');
+      expect(units.msg, 'success');
+      expect(units.data!.length, 3);
+      expect(units.data![0].unitId, 'unit001');
+      expect(units.data![0].unitName, '101');
+      expect(units.data![0].lockIds, ['LOCK001', 'LOCK002']);
+      expect(units.data![2].unitName, 'エントランス');
+      expect(units.data![2].lockIds, null);
     });
   });
 }
